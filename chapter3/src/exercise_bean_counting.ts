98c0bb7f65d170859e1be7c6c90f394a714556f8
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-01 16:39:02 +0900

    [Chapter03] exercise_bean_counting.ts added.

---
+++ chapter3/src/exercise_bean_counting.ts
@@ -0,0 +1,14 @@
+function countBs(word: string): number {
+    return countChar(word, "B");
+  }
+  
+  function countChar(word: string, target: string): number {
+    let count = 0;
+    for (let pos = 0; pos <= word.length; pos++) {
+      if (word[pos] == target) count += 1;
+    }
+    return count;
+  }
+  
+
+console.log("The number of \"B\" in \"Bob\": " + countBs("Bob"));
